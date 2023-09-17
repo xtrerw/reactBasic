ca24c713a1e525226eab69e83869c8f680df7c8f
wei <miguelxu124@.gmail.com>
2023-09-17 18:42:15 +0200

    tipo de routeres

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter } from "react-router-dom";
+import { StaticRouter } from 'react-router-dom/server.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <StaticRouter location='/libros'> */}
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    {/* </StaticRouter> */}
   </React.StrictMode>,
 )
