7659402ac126ff36a21859ec5ce4ee372e5d416d
wei <miguelxu124@.gmail.com>
2023-09-24 23:34:51 +0200

    libro 3

--- src/pagina/BookLayout.jsx
+++ src/pagina/BookLayout.jsx
@@ -1,7 +1,9 @@
+import { useState } from "react"
 import { Link, Outlet } from "react-router-dom"
 
 
 export function BookLayout() {
+    const [num, setNumero]= useState(3)
     return (
         <>
             <Link to='/libros/1'>libro 1</Link>
@@ -9,7 +11,14 @@ export function BookLayout() {
             <Link to='/libros/2'>libro 2</Link>
             <br />
             <Link to='/libros/nuevo'>libro nuevo</Link>
-            <Outlet/>
+            <br />
+            <Link to={`/libros/${num}`}>libro {num}</Link>
+            <Outlet context={{hello: "world"}} />
+            <input 
+            type="number" 
+            value={num}
+            onChange={e=>setNumero(e.target.value)}
+            />
         </>
     )
 }
