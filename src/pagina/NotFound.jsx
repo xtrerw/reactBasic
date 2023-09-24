284dda253d837c11a0da6f1d1c7708f96402fd3e
wei <miguelxu124@.gmail.com>
2023-09-24 23:12:08 +0200

    funcion de devolver la pagina inicial de la pagina de NotFound

--- src/pagina/NotFound.jsx
+++ src/pagina/NotFound.jsx
@@ -1,3 +1,12 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom"
+
 export function NotFound() {
+    const navigate= useNavigate();
+    useEffect(() => {
+        setTimeout(() => {
+            navigate('/');
+        }, 4000);
+    },[]);
     return <h1>404 Not Found</h1>
 }
