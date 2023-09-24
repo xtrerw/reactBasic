9ac1f68349a5470c255198a2d217d167fc2003f0
wei <miguelxu124@.gmail.com>
2023-09-24 23:16:40 +0200

    comentario de función de la pagina de 404 NotFound

--- src/pagina/NotFound.jsx
+++ src/pagina/NotFound.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useNavigate } from "react-router-dom"
 
 export function NotFound() {
+    {/* función de devolver la página inicial por texto "404 not found" */}
     const navigate= useNavigate();
     useEffect(() => {
         setTimeout(() => {
