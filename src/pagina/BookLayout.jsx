a134b5a6a625a36a79eec1bdc1b7746662c793b2
wei <miguelxu124@.gmail.com>
2023-09-24 23:46:14 +0200

    crea una función de cambiar el numero atras de text "libro" y de la barra de busqueda

--- src/pagina/BookLayout.jsx
+++ src/pagina/BookLayout.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react"
-import { Link, Outlet } from "react-router-dom"
+import { Link, Outlet, useSearchParams } from "react-router-dom"
 
 
 export function BookLayout() {
-    const [num, setNumero]= useState(3)
+    const[searchParams, setSearchParams] = useSearchParams({n:3});
+    {/*const [num, setNumero]= useState(3)*/}
+    const num = searchParams.get('n');
     return (
         <>
             <Link to='/libros/1'>libro 1</Link>
@@ -17,7 +19,8 @@ export function BookLayout() {
             <input 
             type="number" 
             value={num}
-            onChange={e=>setNumero(e.target.value)}
+            // onChange={e=>setNumero(e.target.value)}
+            onChange={e=>setSearchParams({n : e.target.value})}
             />
         </>
     )
